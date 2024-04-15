e70f15512cba733c30e3558880f83bb42a883a4d
jin <jin200126@gmail.com>
2024-04-15 14:25:02 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>feature브랜치 테스트 한번 해봤슴다</p>
+      <p>feature브랜치 테스트 한번 해봤슴다!</p>
+      <p>feature브랜치 테스트 한번 해봤슴다!</p>
     </div>
   );
 }
